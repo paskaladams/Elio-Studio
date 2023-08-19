4dcaed6c039ad33b38af1e9fe190f10dd80f51fa
Paskal Adams <paskaladams@gmail.com>
2023-08-19 11:18:48 +0700

    Elio Studio

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
 // TOGGLE CLASS ACTIVE
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 // KETIKA HAMBURGER MENU DIKLIK
-document.querySelector("#hamburger-menu").
-onclick = () => {
-  navbarNav.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
-// KLIK DILUAR SIDEBAR UNTUK MENGHILANGKAN NAV
-const hamburger = document.querySelector('#hamburger-menu');
+// TOGGLE CLASS ACTIVE SEARCH FORM
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-document.addEventListener('click', function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault(); // untuk mengatasi bug ketika klik search, pagenya scroll balik ke atas
+};
+
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  shoppingCart.focus();
+  e.preventDefault();
+};
+
+// KLIK DILUAR ELEMEN UNTUK MENGHILANGKAN ELEMEN
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
 
+// MODAL BOX
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// KLIK TOMBOL CLOSE
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
